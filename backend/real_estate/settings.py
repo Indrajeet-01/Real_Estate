28765d9699b8b54719596769a37917f7e6f9bf5d
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2024-01-04 22:49:07 +0530

    contacts

--- backend/real_estate/settings.py
+++ backend/real_estate/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'realtors',
-    'listings'
+    'listings',
+    'contacts',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +93,12 @@ DATABASES = {
     }
 }
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'indrajeetsinghyadav4@gmail.com'
+EMAIL_HOST_PASSWORD = 'ewspguhxfdbblkpj'
+EMAIL_USE_TLS = True
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 

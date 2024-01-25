e332623122748312d06a5ec9c8a6c6c476a8aae8
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2024-01-25 14:51:54 +0530

    user accounts

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserAccount
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_active', 'is_staff')
+    list_editable = ('is_active', 'is_staff')
+
+admin.site.register(UserAccount, UserAdmin)
 
 # Register your models here.
